fd0cc7b73f81e8da9239124640960f2ddf4cf6cc
dkhansemand <dkhansemand@gmail.com>
2017-02-11 21:27:57 +0100

    Added show posts on spcific profile

--- posts.php
+++ posts.php
@@ -1,9 +1,15 @@
 <?php
+if(isset($_GET["profileId"]) && $_GET["profileId"] != ''){
+    $profilePosts = "WHERE posts.submittedby = " . $_GET["profileId"];
+}else{
+    $profilePosts = "";
+}
 $conn = new dbconnector();
 $query = $conn->newQuery("SELECT posts.title, posts.post, posts.postPicture, DATE_FORMAT(posts.datecreated, '%d-%m-%Y %h:%i:%s') AS postDate, pictures.filename AS postPicture, 
                         userdetails.firstname, userdetails.surname FROM posts 
                         LEFT JOIN pictures ON posts.postPicture = pictures.id 
                         INNER JOIN userdetails ON posts.submittedby = userdetails.userid
+                        $profilePosts
                         ORDER BY posts.datecreated DESC");
 $query->bindParam(':ID', $_SESSION["id"], PDO::PARAM_STR);
 if($query->execute() && $query->rowCount() > 0){
